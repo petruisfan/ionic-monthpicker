0307e58f02cf2fccc807e218a4fb4943bb85cad4
Petru Isfan <petru.isfan@gmail.com>
2016-04-09 15:41:03 +0300

    Updated documentation & bugfix

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # ionic-monthpicker
 Monthpicker for ionic
+
+![Example](/screenshots/screenshot.png?raw=true)
+
+## How to use
+
+1) Install it:
+
+```shell
+$ bower install ionic-monthpicker --save
+```
+
+2) Add the js to your html:
+
+```html
+<script src="lib/ionic-monthpicker/dist/ionic-monthpicker.min.js"></script>
+```
+
+3) Import in your controller:
+
+```JavaScript
+angular.module('app', ['ionic', 'ionic-monthpicker'])
+    .controller(function( MonthPicker ) {
+        MonthPicker.init({});
+        $scope.buttonTap = function() {
+            MonthPicker.show(function(res) {
+                console.log(res);
+            });
+        }
+    });
+```
+
+## Available options:
+
+| Key  | Type | Defaul | Why? |
+| ---- | ---- | ------ | ---- |
+|minMonthIndex | int | 0 | Minimim month index |
+|minYear | int | new Date().getFullYear() | Minimum year |
+|maxMonthIndex | int | new Date().getMonth() | Maximum month index |
+|maxYear | int | new Date().getFullYear() | Maximum year | 
+|monthLabels | array | ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Now', 'Dec'] | Month labels to display |
+|title | string | "Select month" | Popup title |
+|cancelText | string | "Cancel" | Cancel button text |
--- dist/ionic-monthpicker.js
+++ dist/ionic-monthpicker.js
@@ -11,26 +11,41 @@
         var scope;
 
         return {
+            /**
+             * Call for initialization
+             * @param options
+             */
             init: function(options) {
                 scope = $rootScope.$new();
-
+                //
+                // Options and defaults
+                //
                 scope.minMonthIndex = options.minMonthIndex || 0;
                 scope.minYear = options.minYear || new Date().getFullYear();
                 scope.maxMonthIndex = options.maxMonthIndex || new Date().getMonth();
                 scope.maxYear = options.maxYear || new Date().getFullYear();
 
-                scope.monthLabels = options.monthLabels;
+                scope.monthLabels = options.monthLabels || ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Now', 'Dec'];
                 scope.title = options.title || "Select month";
                 scope.cancelText = options.cancelText || "Cancel";
-
+                //
+                // What user selects
+                //
                 scope.selection = {};
 
                 scope.selection.year = scope.maxYear;
-
+                /**
+                 * Arrow buttons
+                 * @param index
+                 */
                 scope.changeYear = function(index) {
                     scope.selection.year += index;
                 };
-
+                /**
+                 * verify month index is in required interval
+                 * @param index
+                 * @returns {boolean}
+                 */
                 scope.isValidMonth = function(index) {
                     var result = (index < scope.minMonthIndex && scope.minYear == scope.selection.year) ||
                         ( index > scope.maxMonthIndex && scope.selection.year == scope.maxYear) ||
@@ -39,7 +54,15 @@
                     return ! result;
                 }
             },
+            /**
+             * Show the month picker
+             * @param callback
+             */
             show: function(callback) {
+                /**
+                 * Tap on month
+                 * @param index
+                 */
                 scope.selectMonth = function(index) {
                     if (scope.isValidMonth(index)) {
                         console.log("valid");
@@ -47,7 +70,9 @@
                         popup.close();
                     }
                 };
-
+                //
+                // Actual popup
+                //
                 var popup = $ionicPopup.show({
                     templateUrl: "monthpicker.html",
                     title: scope.title,
